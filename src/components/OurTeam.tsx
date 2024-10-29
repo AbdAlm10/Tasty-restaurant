3fa44061971f5bd552980130bd68c7e666c3aaab
Abd-Alm <avoided.sy@.com>
2024-10-29 14:42:30 +0300

    Fetching the chefs data

--- src/components/OurTeam.tsx
+++ src/components/OurTeam.tsx
@@ -11,15 +11,14 @@ import {
   Text,
   VStack,
 } from "@chakra-ui/react";
-
 import colors from "../assets/colors/colors";
-import image from "../assets/images/chef1.png";
 import { FaFacebookF, FaLinkedinIn } from "react-icons/fa6";
+import Chefs from "../data/Chefs";
 
 const OurTeam = () => {
   return (
     <Box>
-      <VStack mt={20} fontWeight="bold" fontSize={20}>
+      <VStack textAlign="center" mt={20} fontWeight="bold" fontSize={20}>
         <Text color={colors.mainYello}>Our Chefe</Text>
         <Heading color={colors.black}>Meet Our Expert Chefe</Heading>
       </VStack>
@@ -27,33 +26,36 @@ const OurTeam = () => {
       <Center>
         <SimpleGrid
           columns={{ base: 1, md: 2, lg: 3 }}
-          spacing={{ base: 0, md: 5, lg: 20 }}
+          spacing={{ base: 0, md: 5, lg: 10 }}
         >
-          <Card
-            textAlign="center"
-            borderRadius="20px"
-            my={{ base: 5, md: 3, lg: 10 }}
-          >
-            <CardBody p={0}>
-              <VStack spacing={2} my="6" fontWeight="600">
-                <Heading color={colors.black} fontSize={20}>
-                  Leslie Alexander
-                </Heading>
-                <Text color="gray">Chef Assistant</Text>
+          {Chefs.map((chef, index) => (
+            <Card
+              key={index}
+              textAlign="center"
+              borderRadius="20px"
+              my={{ base: 5, md: 3, lg: 10 }}
+            >
+              <CardBody p={0}>
+                <VStack spacing={2} my="6" fontWeight="600">
+                  <Heading color={colors.black} fontSize={20}>
+                    {chef.name}
+                  </Heading>
+                  <Text color="gray">{chef.role}</Text>
 
-                <ButtonGroup>
-                  <Button p={0} bg="none">
-                    <FaLinkedinIn color={colors.mainYello} />
-                  </Button>
+                  <ButtonGroup>
+                    <Button p={0} bg="none">
+                      <FaLinkedinIn color={colors.mainYello} />
+                    </Button>
 
-                  <Button p={0} ml="0" bg="none">
-                    <FaFacebookF color={colors.mainYello} />
-                  </Button>
-                </ButtonGroup>
-              </VStack>
-              <Image boxSize={300} src={image} />
-            </CardBody>
-          </Card>
+                    <Button p={0} ml="0" bg="none">
+                      <FaFacebookF color={colors.mainYello} />
+                    </Button>
+                  </ButtonGroup>
+                </VStack>
+                <Image boxSize={300} src={chef.imageSrc} />
+              </CardBody>
+            </Card>
+          ))}
         </SimpleGrid>
       </Center>
     </Box>
---
+++ src/data/Chefs.ts
@@ -0,0 +1,10 @@
+import chef1 from "../assets/images/chef1.png";
+import chef2 from "../assets/images/chef2.png";
+import chef3 from "../assets/images/chef3.png";
+
+const Chefs = [
+  { name: "Ralph Edwards", role: "Chef Lead", imageSrc: chef1 },
+  { name: "Leslie Alexander", role: "Chef Assistant", imageSrc: chef2 },
+  { name: "Ronald Richards", role: "Chef Assistant", imageSrc: chef3 },
+];
+export default Chefs;
