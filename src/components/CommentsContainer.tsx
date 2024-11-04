40312778d1aba93fc4c3e4889e9d5bd50f7563a2
Abd-Alm <avoided.sy@.com>
2024-11-04 22:47:33 +0300

    Build the opinions cards

---
+++ src/components/CommentsContainer.tsx
@@ -0,0 +1,49 @@
+import {
+  Card,
+  CardBody,
+  CardFooter,
+  Heading,
+  HStack,
+  Image,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+import { FaQuoteLeft } from "react-icons/fa6";
+import colors from "../assets/colors/colors";
+
+interface Props {
+  text: string;
+  name: string;
+  userName: string;
+  userImageSrc: string;
+}
+
+const CommentsContainer = (props: Props) => {
+  return (
+    <Card
+      borderTop="2px"
+      borderTopColor={colors.mainYello}
+      p={5}
+      borderRadius="20px"
+      bg="white"
+    >
+      <CardBody>
+        <FaQuoteLeft size={60} color="#d5d5d5" />
+        <Text fontSize={25} fontWeight={600} color={colors.black}>
+          {props.text}
+        </Text>
+      </CardBody>
+      <CardFooter p={2}>
+        <HStack>
+          <Image src={props.userImageSrc} w={10} h={10} borderRadius="50%" />
+          <VStack alignItems="start" spacing={0}>
+            <Heading fontSize={20}>{props.name}</Heading>
+            <Text fontSize={15}>{props.userName}</Text>
+          </VStack>
+        </HStack>
+      </CardFooter>
+    </Card>
+  );
+};
+
+export default CommentsContainer;
--- src/components/Rating.tsx
+++ src/components/Rating.tsx
@@ -1,33 +1,51 @@
-import { Box, Button, Text } from "@chakra-ui/react";
-import image from "../assets/images/background2.webp";
+import { Box, GridItem, SimpleGrid, VStack } from "@chakra-ui/react";
 import colors from "../assets/colors/colors";
+import CommentsContainer from "./CommentsContainer";
+import SectionsTitle from "./SectionsTitle";
 
 const Rating = () => {
   return (
-    <Box
-      w="100%"
-      h={{ base: "650px", md: "800px", lg: "550px" }}
-      display="flex"
-      alignItems="center"
-      justifyContent="center"
-      backgroundImage={image}
-      backgroundSize="cover"
-      p={{ base: 5, lg: 10 }}
-    >
-      <Button>Click</Button>
-      <Box
-        borderTop="2px"
-        borderTopColor={colors.mainYello}
-        w="700px"
-        h="400px"
-        p={5}
-        borderRadius="20px"
-        bg={colors.beige}
-      >
-        <Text>sd</Text>
+    <VStack alignItems="center" justifyContent="center" p={{ base: 5, lg: 10 }}>
+      <Box bg={colors.beige} borderRadius="20px" p={5}>
+        <SectionsTitle
+          title={"Testimonials"}
+          description={"What our Clients Say"}
+        />
       </Box>
-      <Button>Click</Button>
-    </Box>
+
+      <SimpleGrid columns={{ base: 1, lg: 3 }} spacing={5}>
+        <GridItem>
+          <CommentsContainer
+            text={
+              "Absolutely loved the food! Every dish was a delight, and the service was fantastic. Will definitely be coming back!"
+            }
+            name={"James Miller"}
+            userName={"@jamesmiller88"}
+            userImageSrc={""}
+          />
+        </GridItem>
+        <GridItem>
+          <CommentsContainer
+            text={
+              "The perfect spot for a cozy dinner. Delicious food, friendly staff, and a great atmosphere!"
+            }
+            name={"David Lopez"}
+            userName={"@davidlopez34"}
+            userImageSrc={""}
+          />
+        </GridItem>
+        <GridItem>
+          <CommentsContainer
+            text={
+              "Amazing flavors and quick service! One of the best dining experiences I've had in a long time."
+            }
+            name={"Alex Chen"}
+            userName={"@alexchen91"}
+            userImageSrc={""}
+          />
+        </GridItem>
+      </SimpleGrid>
+    </VStack>
   );
 };
 
