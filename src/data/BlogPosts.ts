e30fc033a01b8e3b3d81303963ca4819296bec42
Abd-Alm <avoided.sy@.com>
2024-11-05 20:42:02 +0300

    Create a dataBase for blog

---
+++ src/data/BlogPosts.ts
@@ -0,0 +1,32 @@
+import post1 from "../assets/images/BlogImage/blog1.png";
+import post2 from "../assets/images/BlogImage/blog2.png";
+import post3 from "../assets/images/BlogImage/blog3.png";
+
+const BlogPosts = [
+  {
+    imageSrc: post1,
+    date: "25",
+    month: "Dec",
+    auther: "Abdulrahman",
+    gategory: "lunch",
+    title: "The best ways to prepare grilled steak",
+  },
+  {
+    imageSrc: post2,
+    date: "24",
+    month: "Dec",
+    auther: "Sona",
+    gategory: "sweets",
+    title: "From your home, prepare this easy-to-make strawberry dessert.",
+  },
+  {
+    imageSrc: post3,
+    date: "19",
+    month: "Dec",
+    auther: "Napil",
+    gategory: "health",
+    title: "How to be a vegetarian the right way, steps to better health",
+  },
+];
+
+export default BlogPosts;
