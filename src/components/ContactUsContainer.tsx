0f184cbfa9ac238adb865c3c6dcc2b6a93474acd
Abd-Alm <avoided.sy@.com>
2024-11-16 19:22:42 +0300

    Build the contact cards

---
+++ src/components/ContactUsContainer.tsx
@@ -0,0 +1,55 @@
+import {
+  Box,
+  Card,
+  CardBody,
+  Center,
+  Heading,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+import { ReactNode } from "react";
+import colors from "../assets/colors/colors";
+
+interface Props {
+  icon: ReactNode;
+  title: string;
+  description: string;
+}
+
+const ContactUsContainer = ({ icon, title, description }: Props) => {
+  return (
+    <Card
+      borderRadius="30px"
+      bg="white"
+      border="1px solid"
+      borderColor={colors.mainYello}
+    >
+      <CardBody alignItems="center" textAlign="center">
+        <Center>
+          <Box
+            border="1px solid"
+            borderColor={colors.mainBrown}
+            justifyContent="center"
+            alignItems="center"
+            display="flex"
+            h="50px"
+            borderRadius="20px"
+            w="50px"
+            p={2}
+            bg={colors.beige}
+          >
+            {icon}
+          </Box>
+        </Center>
+        <VStack my={5}>
+          <Heading fontSize="25" color={colors.black}>
+            {title}
+          </Heading>
+          <Text color="gray">{description}</Text>
+        </VStack>
+      </CardBody>
+    </Card>
+  );
+};
+
+export default ContactUsContainer;
--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,5 +1,52 @@
+import { Box, GridItem, HStack, SimpleGrid } from "@chakra-ui/react";
+import colors from "../assets/colors/colors";
+import ContactUsContainer from "../components/ContactUsContainer";
+import { MdEmail, MdPlace } from "react-icons/md";
+import { FaClock, FaPhoneAlt } from "react-icons/fa";
+
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <Box
+      alignItems="center"
+      justifyContent="center"
+      display="flex"
+      p={10}
+      bg={colors.beige}
+    >
+      <HStack>
+        <SimpleGrid columns={{ base: 1, md: 2, lg: 4 }} spacing={7}>
+          <GridItem>
+            <ContactUsContainer
+              icon={<MdPlace size="30px" />}
+              title={"Our Address"}
+              description={"4517 Washington Ave. Manchester, Kentucky 39495"}
+            />
+          </GridItem>
+          <GridItem>
+            <ContactUsContainer
+              icon={<MdEmail size="30px" />}
+              title={"info@exmple.com"}
+              description={"Email us anytime for any kind ofquety."}
+            />
+          </GridItem>
+          <GridItem>
+            <ContactUsContainer
+              icon={<FaPhoneAlt size="30px" />}
+              title={"Hot: +208-666-01112"}
+              description={"24/7/365 priority Live Chat and ticketing support."}
+            />
+          </GridItem>
+          <GridItem>
+            <ContactUsContainer
+              icon={<FaClock size="30px" />}
+              title={"Opening Hour"}
+              description={"Sunday-Fri: 9 AM - 6 PM Saturday: 9 AM - 4 PM"}
+            />
+          </GridItem>
+        </SimpleGrid>
+      </HStack>
+    </Box>
+  );
 };
 
 export default Contact;
