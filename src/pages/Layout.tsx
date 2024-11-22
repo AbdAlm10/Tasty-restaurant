b50d663868f4ad091ec4934c18a5db219398ce05
Abd-Alm <avoided.sy@.com>
2024-11-22 19:53:20 +0300

    Added ScrollToTop component to Layout

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../components/Footer";
 import NavBar from "../components/NavBar/NavBar";
+import ScrollToTop from "../ScrollToTop";
 
 const Layout = () => {
   return (
     <>
+      <ScrollToTop />
       <NavBar />
       <Outlet />
       <Footer />
